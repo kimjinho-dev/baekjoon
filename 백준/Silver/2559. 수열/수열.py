703e96cfaf101c256ac3691db8af30f067f4c107
kimjinho-dev <109258271+kimjinho-dev@users.noreply.github.com>
2022-08-17 22:48:42 +0900

    [Silver III] Title: 수열, Time: 116 ms, Memory: 39572 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,11 +1,14 @@
 N, K = map(int, input().split())
 temp = list(map(int, input().split()))
+
+# 슬라이딩 윈도우 알고리즘
 temp_sum = sum(temp[:K])
 max_sum = temp_sum
 
 for idx in range(1, N-K+1):
     temp_sum -= temp[idx-1]
     temp_sum += temp[idx+K-1]
+
     if max_sum < temp_sum:
         max_sum = temp_sum
 
