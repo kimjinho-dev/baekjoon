aba8f064b020a34efb69060a2f42ead04255b36d
kimjinho-dev <109258271+kimjinho-dev@users.noreply.github.com>
2022-08-26 22:51:29 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,26 +1,3 @@
-def solution(word):
-    word = list(word)
-    nums = []
-    while word:
-        num = int(word.pop(0))
-        if word[0] == '0':
-            word.pop(0)
-            num = 10
-        op = word.pop(0)
-
-        if op == 'D':
-            num **= 2
-        elif op == 'T':
-            num **= 3
-
-        if word and word[0] in '*#':
-            bonus = word.pop(0)
-            if bonus == '*':
-                if nums:
-                    nums[-1] *= 2
-                num *= 2
-            elif bonus == '#':
-                num *= -1
-
-        nums.append(num)
-    return sum(nums)
+def solution(dartResult):
+    answer = 0
+    return answer
