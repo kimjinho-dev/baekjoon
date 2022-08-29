69bb6bae8a5eee331435039d1f39d2943a0787f6
kimjinho-dev <109258271+kimjinho-dev@users.noreply.github.com>
2022-08-29 22:34:20 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 161 ms, Memory: 58,524 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -1,14 +1,14 @@
-def chk(x, y, rc):
+def chk(x, y, rc):   # col, row 함수로 합치기
     global result
     if maps[x][y] == '1':
         rc += 1
         if rc == K:
-            result += 1
-        elif rc == K + 1:
+            result += 1     # 알맞은 칸이 나오면, 일단 result += 1
+        elif rc == K + 1:   # 만약 그 다음에도 빈칸이 아니라면, result -= 1
             result -= 1
     else:
-        rc = 0    
-    return rc
+        rc = 0              # 빈칸이 나오면 카운터 초기화
+    return rc               # row,col 최신화
 
 
 for tc in range(1, int(input())+1):
@@ -20,7 +20,7 @@ for tc in range(1, int(input())+1):
         row = 0
         col = 0
         for j in range(N):
-            row = chk(i, j, row)
-            col = chk(j, i, col)
+            row = chk(i, j, row)  # chk함수로 가로 검사. row 값 갱신
+            col = chk(j, i, col)  # chk함수로 세로 검사. col 값 갱신
 
     print(f'#{tc} {result}')
