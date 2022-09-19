9b9df7722c430832a7e8c1863b112a14999462b1
kimjinho-dev <109258271+kimjinho-dev@users.noreply.github.com>
2022-09-19 17:13:14 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,15 @@
+def chk_liars():
+    for i in range(8):
+        for j in range(i+1,9):
+            if keys[i]+keys[j] == sum_liars:
+                del keys[j]
+                del keys[i]
+                return
+
+
+keys = [int(input()) for _ in range(9)]
+sum_liars = sum(keys) - 100
+keys.sort()
+chk_liars()
+for key in keys:
+    print(key)
