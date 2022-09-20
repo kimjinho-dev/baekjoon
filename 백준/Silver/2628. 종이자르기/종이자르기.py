e7a616a341929762bde2219b0d28e30e9a4dedf0
kimjinho-dev <109258271+kimjinho-dev@users.noreply.github.com>
2022-09-21 01:59:41 +0900

    [Silver V] Title: 종이자르기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/2628. 종이자르기/종이자르기.py
@@ -0,0 +1,18 @@
+N,M = map(int,input().split())
+rows = [0,N]
+cols = [0,M]
+for _ in range(int(input())):
+    op, pos = map(int,input().split())
+    if op == 0:
+        cols.append(pos)
+    else:
+        rows.append(pos)
+cols.sort()
+rows.sort()
+result = 0
+for idx in range(len(rows)-1):
+    for idx2 in range(len(cols)-1):
+        width = (rows[idx+1] - rows[idx]) * (cols[idx2+1] - cols[idx2])
+        if result < width:
+            result = width
+print(result)
