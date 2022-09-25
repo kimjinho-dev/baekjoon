1649665625b678439130b167c2dd1849fb794a65
kimjinho-dev <109258271+kimjinho-dev@users.noreply.github.com>
2022-09-25 23:10:57 +0900

    [Unrated] Title: [모의 SW 역량테스트] 특이한 자석, Time: 223 ms, Memory: 59,712 KB -BaekjoonHub

---
+++ SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+def f(num,rotation,pos):
+    if 0 <= num < 4 and maps[num][2 * -1 * pos] != maps[num-pos][2 * pos]:
+        f(num+pos, rotation * -1, pos)
+
+        if rotation == 1:
+            temp = maps[num].pop()
+            maps[num].appendleft(temp)
+        else:
+            temp = maps[num].popleft()
+            maps[num].append(temp)
+
+for test_case in range(1,int(input())+1):
+    actions = int(input())
+    maps = [deque(map(int,input().split())) for _ in range(4)]
+    for _ in range(actions):
+        num, rotation = map(int,input().split())
+        num -= 1
+        f(num-1, rotation * -1, -1)
+        f(num+1, rotation * -1, 1)
+
+        if rotation == 1:
+            temp = maps[num].pop()
+            maps[num].appendleft(temp)
+        else:
+            temp = maps[num].popleft()
+            maps[num].append(temp)
+    
+    result = maps[0][0]+maps[1][0]*2+maps[2][0]*4+maps[3][0]*8
+    print(f'#{test_case} {result}')
