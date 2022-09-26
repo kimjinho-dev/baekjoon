c048aa9cb45228131c00b30e5616bb3688a5c672
kimjinho-dev <109258271+kimjinho-dev@users.noreply.github.com>
2022-09-26 13:31:29 +0900

    [Bronze I] Title: 색종이, Time: 436 ms, Memory: 123120 KB -BaekjoonHub

---
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -0,0 +1,14 @@
+N = int(input())
+maps = [[-1]*1001 for _ in range(1001)]
+command = [list(map(int,input().split())) for _ in range(N)]
+result = [0]*N
+for k in range(N-1,-1,-1):
+    x, y, width, height = command[k]
+    for i in range(x,x+width):
+        for j in range(y,y+height):
+            if maps[i][j] == -1:
+                maps[i][j] = k
+                result[k] += 1
+
+for r in result:
+    print(r)
